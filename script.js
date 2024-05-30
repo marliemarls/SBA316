fb7b5147a1e9f612996be8550bba4577d332fe3b
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 10:10:47 -0400

    changed header creation site and now using querySelector instead of creating header in js with createElement

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const newHeader = document.createElement("h1");
-newHeader.textContent = "Harawiq Records";
-document.body.appendChild(newHeader)
-
+//changed my header from being created in js to being already created in HTML so that i could querySelect the element and manipulate it from here
+const newHeader = document.querySelector("h1");
+newHeader.textContent = `Welcome!`;
 newHeader.addEventListener("click", (event)=>{
-    newHeader.style.color = "cornflowerblue"
-    
-})
+    newHeader.style.color = "cornflowerblue";
+});
+
+const photosDiv = document.getElementById("photos-div");
+photosDiv.innerText = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
 
 
-// const userName = prompt("whats your name?");
+
+
+
+// 
 // console.log(userName)
 
  
