143150c1b9501715b8a8b9eae7a2025940ba68d6
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 16:27:29 -0400

    adding more elements

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const newHeader = document.querySelector("h1");
 newHeader.textContent = `Welcome!`;
 newHeader.addEventListener("click", (event)=>{
     newHeader.style.color = "cornflowerblue";
+    window.alert("You found the hidden feature!")
 });
+
+const navBar = document.createElement("div");
+navBar.innerHTML = "navbar"
+
 
 const blogDiv = document.getElementById("blog-paragraph")
 blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
