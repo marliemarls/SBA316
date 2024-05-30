a67a4d2206aae97aa5422bafc0d72f02fe2d3fac
Marlie Martinez <marlieahue@gmail.com>
2024-05-29 21:13:18 -0400

    event listener

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const newHeader = document.createElement("h1");
 newHeader.textContent = "Harawiq Records";
 document.body.appendChild(newHeader)
+
+newHeader.addEventListener("click", (event)=>{
+    newHeader.style.color = "cornflowerblue"
+    
+})
+
+
+// const userName = prompt("whats your name?");
+// console.log(userName)
+
+ 
