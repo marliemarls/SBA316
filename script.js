8d799f663ea14a2515e66414634e5b1e358ad711
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 12:49:22 -0400

    ugh working not sure if i even like this

--- script.js
+++ script.js
@@ -8,16 +8,12 @@ newHeader.addEventListener("click", (event)=>{
 const blogDiv = document.getElementById("blog-paragraph")
 blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-// function createLinks(arr){
-//     arr.forEach(element => {
-//         let newLinks = document.createElement("a");
-//         newLinks.innerHTML = element;
-//     });
-    
+//creating an array to use to be able to create the form 
+// const forms = document.getElementsByName("form");
 
-// }
 
-// createLinks()
+
+
 
 
 // 
