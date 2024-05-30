bc6ac5a289b387d6611ee75c50280188125c6434
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 11:34:08 -0400

    implemented a photo, ipsum lorem bio, and a music player

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ newHeader.addEventListener("click", (event)=>{
     newHeader.style.color = "cornflowerblue";
 });
 
+const blogDiv = document.getElementById("blog-paragraph")
+blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+// function createLinks(arr){
+//     arr.forEach(element => {
+//         let newLinks = document.createElement("a");
+//         newLinks.innerHTML = element;
+//     });
+    
 
+// }
+
+// createLinks()
 
 
 // 
