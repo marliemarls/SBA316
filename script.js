a5c40c20009b37a0422556253748764e48017b5a
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 21:03:42 -0400

    added more stuff to the event listener and working on updating the javascript file to be up to date with the names of the divs

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //changed my header from being created in js to being already created in HTML so that i could querySelect the element and manipulate it from here
 const newHeader = document.querySelector("h1");
-newHeader.textContent = `Welcome!`;
+newHeader.textContent = `Harawiq Records`;
+
 newHeader.addEventListener("click", (event)=>{
-    newHeader.style.color = "cornflowerblue";
-    window.alert("You found the hidden feature!")
+    const newColor = window.prompt("What is your favorite color?").replace(/\s/g, '');
+    newHeader.style.color = `${newColor}`;
+    setTimeout(()=>window.alert("You found the hidden feature, congratulations!"),3000)
 });
 
-const navBar = document.createElement("div");
-navBar.innerHTML = "navbar"
+
+// const navBar = document.createElement("div");
+// navBar.innerHTML = "navbar"
 
 
-const blogDiv = document.getElementById("blog-paragraph")
-blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// const blogDiv = document.getElementById("blog-paragraph")
+// blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 
 
