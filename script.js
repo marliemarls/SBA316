d7e427173dca1d73ea7d04dbe494532e44f9e96d
Marlie Martinez <marlieahue@gmail.com>
2024-05-31 15:47:10 -0400

    added a photo to the music div and included more information about the song

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //changed my header from being created in js to being already created in HTML so that i could querySelect the element and manipulate it from here
+
 const newHeader = document.querySelector("h1");
 newHeader.textContent = `Harawiq Records`;
 
@@ -9,19 +10,19 @@ newHeader.addEventListener("click", (event)=>{
 });
 
 
-// const navBar = document.createElement("div");
-// navBar.innerHTML = "navbar"
-
 
 const photoParagraph = document.getElementById("video-paragraph")
 photoParagraph.innerText = "We Are Still Here, is a music video for the song written by artist, Bobby Sanchez. This short film features many other trans, Indigenous, Two-Spirit folks from Oakland, California. Filmed by Marlie Martinez."
 
-const navDiv = document.getElementsByClassName("nav_and_header")
-console.log(navDiv)
-for(let i = 0; i < navDiv.length; i++){
-    const newNav = document.createElement("a")
-    
-}
+const navDiv = document.getElementsByClassName("nav_and_header");
+
+
+// createNavBar(navDiv)
+const forms = document.getElementsByTagName("form");
+console.log(forms)
+// forms.addEventListener("submit", (e) => {
+//     console.log(e)
+// })
 
 
  
