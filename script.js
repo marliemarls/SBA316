1e6b820c21805e9708bd07b4dbcda30c29bc6ba1
Marlie Martinez <marlieahue@gmail.com>
2024-05-30 21:05:56 -0400

    added lorem ipsum

--- script.js
+++ script.js
@@ -13,17 +13,8 @@ newHeader.addEventListener("click", (event)=>{
 // navBar.innerHTML = "navbar"
 
 
-// const blogDiv = document.getElementById("blog-paragraph")
-// blogDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+const photoParagraph = document.getElementById("photo-paragraph")
+photoParagraph.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-
-
-
-
-
-
-
-// 
-// console.log(userName)
 
  
